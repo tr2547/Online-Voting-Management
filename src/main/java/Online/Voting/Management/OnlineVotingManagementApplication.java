fa3bcd9261f9949e2c8a9fda0a792482559bebb6
tr2547 <tr-myaka@wiu.edu>
2024-06-19 23:30:45 -0500

    First commit

---
+++ src/main/java/Online/Voting/Management/OnlineVotingManagementApplication.java
@@ -0,0 +1,13 @@
+package Online.Voting.Management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVotingManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVotingManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Online/Voting/Management/OnlineVotingManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package Online.Voting.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineVotingManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
